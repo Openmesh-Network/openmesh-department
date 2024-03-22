d1b72e58eb389cfd0ff12378da255a1b47c45367
Plopmenz <plopmenz@gmail.com>
2024-03-23 00:17:09 +0100

    department save tagVoting plugin address

--- deploy/internal/Department.ts
+++ deploy/internal/Department.ts
@@ -4,12 +4,18 @@ export interface DeployDepartmentSettings
   extends Omit<ExecuteInfo, "abi" | "to" | "function" | "args"> {
   name: string;
   departmentFactory: Address;
+  pluginSetupProcessor?: Address;
+}
+
+export interface DeployDepartmentReturn {
+  dao: Address;
+  tagVoting: Address;
 }
 
 export async function deployDepartment(
   deployer: Deployer,
   settings: DeployDepartmentSettings
-): Promise<Address> {
+): Promise<DeployDepartmentReturn> {
   const tag = deployer.viem.keccak256(deployer.viem.toBytes(settings.name));
   const metadata = "";
   const { receipt } = await deployer.execute({
@@ -20,18 +26,34 @@ export async function deployDepartment(
     ...settings,
   });
 
-  const events = await deployer.getEvents({
+  const departmentCreatedEvents = await deployer.getEvents({
     abi: "DepartmentFactory",
     address: settings.departmentFactory,
     eventName: "DepartmentCreated",
     logs: receipt.logs,
   });
-
-  if (events.length === 0) {
+  if (departmentCreatedEvents.length === 0) {
     throw new Error("DepartmentCreated event not emitted");
   }
+  const dao = (
+    departmentCreatedEvents[0].args as any as { department: Address }
+  ).department;
 
-  const department = (events[0].args as any as { department: Address })
-    .department;
-  return department;
+  const installationPreparedEvents = await deployer.getEvents({
+    abi: "PluginSetupProcessor",
+    address: settings.pluginSetupProcessor,
+    eventName: "InstallationPrepared",
+    logs: receipt.logs,
+  });
+  if (installationPreparedEvents.length === 0) {
+    throw new Error("InstallationPrepared event not emitted");
+  }
+  const tagVoting = (
+    installationPreparedEvents[0].args as any as { plugin: Address }
+  ).plugin;
+
+  return {
+    dao: dao,
+    tagVoting: tagVoting,
+  };
 }
